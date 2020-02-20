2f6dba962dc87398b80866c8fe3ee49e3c006309
Andrea <agdiiura@users.noreply.github.com>
2020-02-20 14:02:55 +0100

    Update loader.sh

--- assets/loader.sh
+++ assets/loader.sh
@@ -3,5 +3,7 @@
 
 svn checkout https://github.com/huynhsamha/quick-mongo-atlas-datasets/trunk/dump/sample_airbnb
 
+svn checkout https://github.com/huynhsamha/quick-mongo-atlas-datasets/trunk/dump/sample_mflix
+
 pip3 install pymongo
 python3 loader.py
