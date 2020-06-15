148fcdb6d4871ffcd502e36258dfbe868da84dfb
dcc-sapienza <dcc-sapienza@gmail.com>
2020-06-15 11:36:20 +0200

    davide

--- fundamentals-of-linux/fundamentals-of-linux-remote-access/intro.sh
+++ fundamentals-of-linux/fundamentals-of-linux-remote-access/intro.sh
@@ -1,11 +1,11 @@
-# mkdir /home/U1
-# mkdir /home/U2
-# sudo useradd -d /home/U1 -p $(openssl passwd -1 p1) U1
-# sudo useradd -d /home/U2 -p $(openssl passwd -1 p2) U2
-# chown -R U1 /home/U1
-# chown -R U2 /home/U2 
-# ssh -oStrictHostKeyChecking=no root@[[HOST2_IP]] 
-# mkdir /home/W1
-# sudo useradd -d /home/W1 -p $(openssl passwd -1 p1) W1
-# sudo useradd -d /home/W1 -p $(openssl passwd -1 p1) U1
-# exit
+mkdir /home/U1
+mkdir /home/U2
+useradd -d /home/U1 -p $(openssl passwd -1 p1) U1
+useradd -d /home/U2 -p $(openssl passwd -1 p2) U2
+chown -R U1 /home/U1
+chown -R U2 /home/U2 
+ssh -oStrictHostKeyChecking=no root@[[HOST2_IP]] 
+mkdir /home/W1
+useradd -d /home/W1 -p $(openssl passwd -1 p1) W1
+useradd -d /home/U1 -p $(openssl passwd -1 p1) U1
+exit
