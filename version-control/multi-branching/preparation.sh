a57f1d10b14d50572d37c3fa7f891a624cb8bff3
dcc-sapienza <dcc.sapienza@gmail.com>
2020-08-25 14:53:34 +0200

    preparation edited

--- version-control/multi-branching/preparation.sh
+++ version-control/multi-branching/preparation.sh
@@ -2,7 +2,7 @@ git init
 git config color.ui false
 touch installation.txt
 git add installation.txt
-git commit -m "intallation guide created"
-touch user_manual.txt
-git add user_manual.txt
-git commit -m "user manual created"
+git commit -m "installation guide created"
+echo "INSTALLATION GUIDE" >> installation.txt
+git add installation.txt
+git commit -m "installation guide edited"
