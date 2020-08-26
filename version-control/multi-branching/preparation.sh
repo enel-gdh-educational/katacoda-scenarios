fcd25c85a619ce4afad783cc88bceb870b1533a8
dcc-sapienza <dcc.sapienza@gmail.com>
2020-08-26 11:59:54 +0200

    preparation edits

--- version-control/multi-branching/preparation.sh
+++ version-control/multi-branching/preparation.sh
@@ -1,8 +1,11 @@
 git init
 git config color.ui false
+git config user.name "Andrea Massaia"
+git config user.email andreamassaia@katacoda.com
 touch installation.txt
 git add installation.txt
 git commit -m "installation guide created"
+git commit --amend --
 echo "INSTALLATION GUIDE" >> installation.txt
 git add installation.txt
 git commit -m "installation guide edited"
