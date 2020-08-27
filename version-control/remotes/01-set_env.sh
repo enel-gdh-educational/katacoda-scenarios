d857bd7c6572a6ff1f86bf175e1f7bada95c51fd
dcc-sapienza <dcc.sapienza@gmail.com>
2020-08-27 16:53:27 +0200

    envs

--- version-control/remotes/01-set_env.sh
+++ version-control/remotes/01-set_env.sh
@@ -1,4 +1,18 @@
-mkdir /s/remote-location
-git init --bare /s/remote-location/myproject.git
+mkdir /s/remote-location/
+mkdir /s/remote-location/0
+mkdir /s/remote-location/1
 
+git init --bare /s/remote-location/0/myproject.git
+
+cd /s/remote-location/1
+git init
+git remote add origin /s/remote-location/0/myproject.git
+git push origin master
+git branch new_branch
+git checkout new_branch
+touch file_bello.txt
+git add file_bello.txt
+git commit -m "file_bello.txt"
+
+cd /home/scrapbook/tutorial/
 git init
