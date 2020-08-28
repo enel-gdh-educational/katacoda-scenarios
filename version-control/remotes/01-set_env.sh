2baec1c770428e0ead7cdc21092df9682bda7ff4
dcc-sapienza <dcc.sapienza@gmail.com>
2020-08-28 10:26:16 +0200

    correction on step push

--- version-control/remotes/01-set_env.sh
+++ version-control/remotes/01-set_env.sh
@@ -8,6 +8,14 @@ cd /s/remote-location/1
 git init
 git remote add origin /s/remote-location/0/myproject.git
 git push origin master
+git branch additional_feature
+git checkout additional_feature
+echo "print('Hello world')" >> feature.py
+git add feature.py
+git commit -m "hello world feature"
+git push origin additional_feature
+git checkout master
+git pull
 
 cd /home/scrapbook/tutorial/
 git init
