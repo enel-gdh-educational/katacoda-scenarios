8d475cc645286c9f71dd5419033da2734411bf4b
dcc-sapienza <dcc.sapienza@gmail.com>
2020-08-28 12:08:43 +0200

    review of env creation

--- version-control/remotes/01-set_env.sh
+++ version-control/remotes/01-set_env.sh
@@ -7,15 +7,10 @@ git init --bare /s/remote-location/0/myproject.git
 cd /s/remote-location/1
 git init
 git remote add origin /s/remote-location/0/myproject.git
+echo "print('Hello World')" >> base_feature.py
+git add base_feature.py
+git commit -m "first commit"
 git push origin master
-git branch additional_feature
-git checkout additional_feature
-echo "print('Hello world')" >> feature.py
-git add feature.py
-git commit -m "hello world feature"
-git push origin additional_feature
-git checkout master
-git pull
 
 cd /home/scrapbook/tutorial/
 git init
