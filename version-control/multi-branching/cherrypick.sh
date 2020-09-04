d981b4e2a30f62f1f31eed8844830015875d03b3
dcc-sapienza <dcc.sapienza@gmail.com>
2020-09-04 10:05:53 +0200

    cherry-pick preparation edited

--- version-control/multi-branching/cherrypick.sh
+++ version-control/multi-branching/cherrypick.sh
@@ -1,5 +1,7 @@
 touch model.py
-git commit -a -m 'model.py created in the wrong place'
+git add model.py
+git commit -m 'model.py created in the wrong place'
 cat "import pandas" >> model.py
-git commit -a -m 'model.py edited, still in the wrong place'
+git add model.py
+git commit -m 'model.py edited, still in the wrong place'
 git checkout master
