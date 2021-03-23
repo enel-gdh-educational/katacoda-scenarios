1a4be442e0a1695303774dbaa0d5383556648940
dcc-sapienza <riccardo.ghera@enel.com>
2021-03-23 02:39:12 +0100

    linux fundamentals  - minor change

--- fundamentals-of-linux/fundamentals-of-linux-stream-processing/step3-foreground.sh
+++ fundamentals-of-linux/fundamentals-of-linux-stream-processing/step3-foreground.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Waiting for the step 3 configuration to complete. Please wait.."; 
-
 while [ ! -f /opt/.backgroundfinished ] ; do 
     sleep 2;
     printf "." 
 done; 
+
+echo "Waiting for the step 3 configuration to complete. Please wait.."; 
 echo "Done"
